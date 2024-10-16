5e326ae0a1f993a67c9eec6e5ee2724c136ec752
Riccardo <riccardo.pausig@gmail.com>
2024-10-16 12:17:19 +0200

    Add project files.

---
+++ DevOpsTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DevOpsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
